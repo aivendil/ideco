51697c525f07473af83e6ad451d22e691c3329d0
aivendil <aivendil3@yandex.ru>
2019-07-22 21:13:41 +0500

    Add docstring

--- test_task/routes.py
+++ test_task/routes.py
@@ -4,6 +4,11 @@ from test_task.views import get_all_news, get_news_by_id
 
 
 def setup_routes(app):
-
+    """
+    Set routes path.
+    :param app: wsgi application
+    :type app: aiohttp.web_app.Application
+    :return:
+    """
     app.add_routes([web.get('/', get_all_news),
                     web.get(r'/news/{news_id:\d+}', get_news_by_id)])
--- test_task/views.py
+++ test_task/views.py
@@ -1,5 +1,4 @@
 import datetime
-import json
 
 from aiohttp import web
 
@@ -17,11 +16,30 @@ def check_news_available(news):
 
 
 async def find_comments_by_news_id(news_id, comments):
+    """
+    Find all comments, related with given news
+    :param news_id: Id of news
+    :type news_id: int
+    :param comments: List of comment in which will be searched
+    :type comments: list of dict
+    :return: List of found comments
+    :rtype: list of dict
+    """
     filtered = filter(lambda comment: comment['news_id'] == news_id, comments)
     return list(filtered)
 
 
 async def find_news(news_id, all_news, comments):
+    """
+    Find news with given id. In founded news will be added related comments and count of comments.
+    :param news_id: Id of news
+    :type news_id: int
+    :param all_news: List of news in which will be searched
+    :type all_news: list of dict
+    :param comments: List of comment in which will be searched
+    :return: First news with given id if it exist else None.
+    :rtype: dict or None
+    """
     filtered_news = list(filter(lambda news: news['id'] == news_id, all_news))
     if len(filtered_news) == 0:
         return None
@@ -34,6 +52,12 @@ async def find_news(news_id, all_news, comments):
 
 
 async def get_all_news(request):
+    """
+    View for list of all available news.
+    :param request: Http request
+    :type request: aiohttp.web_request.Request
+    :return: Json with list of all news
+    """
     for news in all_news:
         news['comments_count'] = len(await find_comments_by_news_id(news['id'], all_comments))
     filtered_news = list(
@@ -42,6 +66,12 @@ async def get_all_news(request):
 
 
 async def get_news_by_id(request):
+    """
+    View for news with given id.
+    :param request: http request
+    :type request: aiohttp.web_request.Request
+    :return: Json with news if it exist and available else 404
+    """
     news_id = int(request.match_info['news_id'])
     news = await find_news(news_id, all_news, all_comments)
     if news is None or not check_news_available(news):
